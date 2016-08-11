09c9c438a893628e103532b3e80a1cdbbf9323d8
Martin Hlaváč <hlavacm@hotmail.com>
2016-08-11 14:10:21 +0200

    Initialization is now in scope of WP init action
    + added WP_TRACY_ENABLE_MODE and wp_tracy_panels_filter

--- README.md
+++ README.md
@@ -7,8 +7,10 @@ When it's activated, it automatically shows Tracy bar and displays within global
 ## Installation
 
 1. Use command on your path: composer require ktstudio/wp-tracy
-3. Profit!
-4. You can optionally define PHP boolean constant WP_TRACY_CHECK_USER_LOGGED_IN...
+2. Profit!
+3. You can optionally define PHP (boolean) constant WP_TRACY_CHECK_USER_LOGGED_IN to check only logged users...
+4. You can optionally define PHP constant WP_TRACY_ENABLE_MODE to set Tracy\Debugger::enable($mode)...
+5. You can optionally use wp_tracy_panels_filter to modify default panels array (full class names)
 
 ![WP Tracy](https://ktstudio.github.io/images/wp-tracy.png "Tracy bar auto-display after plugin activation")
 ![WP Tracy exception](https://ktstudio.github.io/images/wp-tracy-exception.png "Tracy exception dialog when is occured")
--- src/index.php
+++ src/index.php
@@ -1,27 +1,31 @@
 <?php
 
-if (defined("DOING_AJAX") && DOING_AJAX) {
-    return; // for IE compatibility WordPress media upload
-}
+add_action("init", "wp_tracy_init_action", 1);
 
-if (defined("WP_TRACY_CHECK_USER_LOGGED_IN") && WP_TRACY_CHECK_USER_LOGGED_IN && is_user_logged_in()) {
-    return; // cancel for anonymous users
-}
+function wp_tracy_init_action() {
+    if (defined("DOING_AJAX") && DOING_AJAX) {
+        return; // for IE compatibility WordPress media upload
+    }
 
-Tracy\Debugger::enable(); // hooray, enabling debugging using Tracy
+    if (defined("WP_TRACY_CHECK_USER_LOGGED_IN") && WP_TRACY_CHECK_USER_LOGGED_IN && is_user_logged_in()) {
+        return; // cancel for anonymous users
+    }
 
-// panels in the correct order
-$panels = array(
-    "WpTracy\\WpPanel",
-    "WpTracy\\WpUserPanel",
-    "WpTracy\\WpPostPanel",
-    "WpTracy\\WpQueryPanel",
-    "WpTracy\\WpQueriedObjectPanel",
-    "WpTracy\\WpDbPanel",
-    "WpTracy\\WpRewritePanel",
-);
+    Tracy\Debugger::enable(defined("WP_TRACY_ENABLE_MODE") ? WP_TRACY_ENABLE_MODE : null); // hooray, enabling debugging using Tracy
+    // panels in the correct order
+    $defaultPanels = array(
+        "WpTracy\\WpPanel",
+        "WpTracy\\WpUserPanel",
+        "WpTracy\\WpPostPanel",
+        "WpTracy\\WpQueryPanel",
+        "WpTracy\\WpQueriedObjectPanel",
+        "WpTracy\\WpDbPanel",
+        "WpTracy\\WpRewritePanel",
+    );
+    $panels = apply_filters("wp_tracy_panels_filter", $defaultPanels);
 
-// panels registration
-foreach ($panels as $className) {
-    Tracy\Debugger::getBar()->addPanel(new $className);
+    // panels registration
+    foreach ($panels as $className) {
+        Tracy\Debugger::getBar()->addPanel(new $className);
+    }
 }
